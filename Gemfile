04d8ac11122244a6bfdd2b63ecaa0c990cabe301
Maksim Ind <fss92@pochta.ru>
2013-07-11 02:57:46 +0400

    fix

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'capistrano'
 # gem 'jbuilder'
 
 # Use unicorn as the app server
-# gem 'unicorn'
+gem 'unicorn'
 
 # Deploy with Capistrano
 # gem 'capistrano'
---
+++ vendor/bundle/gems/raindrops-0.11.0/ext/raindrops/siteconf20130711-55263-1ncq8xq.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130711-55263-o53xnu"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
